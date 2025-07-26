9b41001a0c7969dbbd97fa7dc4e353e006d1ecef
Thales Luiz <thalesluizbraga@gmail.com>
2025-07-26 09:37:26 -0300

    second step of the challenge completed

--- bonus.py
+++ bonus.py
@@ -1,4 +1,6 @@
 #%%
+
+# Etapa 1
 # Escreva um programa em Python que solicita ao usuário para 
 # digitar seu nome, o valor do seu salário mensal e o valor 
 # do bônus que recebeu. O programa deve, então, imprimir uma
@@ -6,17 +8,20 @@
 #  do salário em comparação com o bônus recebido.
 
 
+# Etapa 2
+# Integre na solução anterior um fluxo de While que repita o fluxo até que o usuário insira as informações corretas
+
 #%%
-# Function to define 
-def bonus_calculation(name:str, monthly_salary:float, bonus_rate:float) -> float:
-    return f'Hi {str(name)}, your bonus is {float(monthly_salary) * (float(bonus_rate))}'
 
-# %%
+while True:
+    name = str(input('Insira o seu nome:'))
+    if name.isalpha():
+        monthly_salary = float(input('Insira seu salario:'))
+        if isinstance(monthly_salary,float) and monthly_salary > 0:
+            bonus_rate = float(input('Insira o bonus rate:'))
+            if isinstance(bonus_rate, float) and bonus_rate > 0:
+                bonus = monthly_salary * bonus_rate
+                print(f'Seu bonus sera de {bonus}')
+                break
 
-# function call and result
-name = 'Thales'
-monthly_salary = 9750
-bonus_rate = 0.5
 
-bonus_total = bonus_calculation(name=name, monthly_salary=monthly_salary, bonus_rate=bonus_rate)
-print(bonus_total)
