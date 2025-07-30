539b51d6131af4382f2c6af4e3d958dfb6d3c52c
Thales Luiz <thalesluizbraga@gmail.com>
2025-07-30 08:18:12 -0300

    csv_reader.py added

--- csv_reader.py
+++ csv_reader.py
@@ -16,7 +16,35 @@ with open(file_path, mode='r', encoding='utf-8') as file:
     for row in csv_reader:
         print(row)
 
-
-
 #%%
 
+# escrevendo no arquivo com apped (mode='a'), se fosse pra sobrescrever seria mode='w'
+
+import csv
+
+file_path = 'vendas.csv'
+new_data = {'Produto': 'luvas', 'Categoria': 'esportes', 'Quantidade': 10, 'Venda': 60}
+
+# To append to existing file (keeping header if it exists)
+with open(file_path, mode='a', newline='', encoding='utf-8') as file:
+    fieldnames = new_data.keys()    
+    writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')
+    
+    if file.tell() == 0:
+        writer.writeheader()
+    
+    writer.writerow(new_data) # writerows se tivesse mais de uma linha
+
+
+
+# %%
+
+
+import csv
+
+file_path = 'vendas.csv'
+
+with open(file_path, mode='r', encoding='utf-8') as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        print(row)
