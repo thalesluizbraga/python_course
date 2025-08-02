a31e77d5fdd39c0d124199c11f2ff3771a9d6c99
Thales Luiz <thalesluizbraga@gmail.com>
2025-08-01 22:00:15 -0300

    csv_reader.py adjusted

--- csv_reader.py
+++ csv_reader.py
@@ -7,44 +7,48 @@
     # iterar para cada linha do arquivo 
     # printar as linhas do arquivo
 
+# Ler arquivo csv com csv lib
+
 import csv
 
 file_path = 'vendas.csv'
 
-with open(file_path, mode='r', encoding='utf-8') as file:
-    csv_reader = csv.DictReader(file)
-    for row in csv_reader:
-        print(row)
+def ler_csv(file_path):
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            return row
 
+ler_csv(file_path)
 #%%
 
-# escrevendo no arquivo com apped (mode='a'), se fosse pra sobrescrever seria mode='w'
+# adicionar/appendar dados em um csv (mode='a'), se fosse pra sobrescrever seria mode='w'
 
 import csv
 
 file_path = 'vendas.csv'
 new_data = {'Produto': 'luvas', 'Categoria': 'esportes', 'Quantidade': 10, 'Venda': 60}
 
-# To append to existing file (keeping header if it exists)
-with open(file_path, mode='a', newline='', encoding='utf-8') as file:
-    fieldnames = new_data.keys()    
-    writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')
-    
-    if file.tell() == 0:
-        writer.writeheader()
-    
-    writer.writerow(new_data) # writerows se tivesse mais de uma linha
+def appendar_dados_no_arquivo(file_path, new_data):
+    with open(file_path, mode='a', newline='', encoding='utf-8') as file:
+        fieldnames = new_data.keys()    
+        writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')
+        if file.tell() == 0:
+            writer.writeheader()
+        return writer.writerow(new_data) # writerows se tivesse mais de uma linha
 
+appendar_dados_no_arquivo(file_path, new_data)
 
 
 # %%
 
-
-import csv
+# filtrar dados no csv
 
 file_path = 'vendas.csv'
 
 with open(file_path, mode='r', encoding='utf-8') as file:
-    csv_reader = csv.DictReader(file)
-    for row in csv_reader:
-        print(row)
+    reader = csv.DictReader(file)
+    for row in reader: # o dictreader cria um dicioonario para cada linha, entao preciso iterar pra cada dicionario que é cada linha e depois iterar de novo pra achar os valores e chaves
+        for key, value in row.items():
+                print(key[0
+                ])
